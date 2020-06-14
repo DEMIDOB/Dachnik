bbe69e8c51d11c14bc5bb5c01c0522a692157f86
Даниил Демидов <danademidov@MacBook-Air-Daniil.local>
2020-06-14 22:23:12 +0300

    aaa

--- src/feedback/forms.py
+++ src/feedback/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class FeedbackForm(forms.Form):
     name = forms.CharField(label='Имя', max_length=50, required=True)
     topic = forms.CharField(label='Тема обращения', max_length=100, required=True)
-    body = forms.CharField(label='Тело', max_length=1000, required=True)
+    body = forms.CharField(label='Тело', max_length=500, required=True, widget=forms.Textarea)
     email = forms.EmailField(label='E-Mail', required=True)
--- src/products/models.py
+++ src/products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     category    = models.CharField(max_length=150, default='Фрукты')
     price       = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     discount    = models.IntegerField(default=0)
+    producer    = models.CharField(default="Неизвестный", max_length=50)
     icon        = models.CharField(default="http://demidob.site/dch/imgs/none.jpg", max_length=150)
     article     = models.CharField(max_length=25, default='0', primary_key=True)
     amount      = models.IntegerField(default=0)
