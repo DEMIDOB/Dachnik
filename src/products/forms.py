7b4c5afb32e33cd61c96d456107da7a49aae8f74
Даниил Демидов <danademidov@MacBook-Air-Daniil.local>
2020-06-16 13:45:24 +0300

    some mainpage changes

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class OrderProductForm(forms.Form):
-    name = forms.CharField(label='Имя', max_length=50, required=True)
+    name    = forms.CharField(label='Имя', max_length=50, required=True)
     article = forms.CharField(label='article', max_length=, required=True)
-    amount = forms.IntegerField(label='Количество', required=True)
+    amount  = forms.IntegerField(label='Количество', required=True)
     text
-    phone = forms.forms.CharField(label='Телефон (не обязательно)', max_length=25, required=False)
+    phone   = forms.forms.CharField(label='Телефон (не обязательно)', max_length=25, required=False)
