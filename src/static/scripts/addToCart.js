fc2940090fda8f37fbae7925fd5a428d8863863c
DEMIDOB <dania.demidov@yandex.ru>
2020-06-25 19:51:47 +0300

    product details view

--- src/static/scripts/addToCart.js
+++ src/static/scripts/addToCart.js
@@ -2,5 +2,6 @@
 
 function addToCart(article) {
 	amount = document.getElementById('count1').innerHTML;
+	document.getElementById('modalAmount').innerHTML = amount;
 	makeRequest("/cart/add/?article=" + article + "&amount=" + amount);
 }
