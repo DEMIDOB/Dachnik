0037fd488843b2b5c535c87e551b583a06601088
DEMIDOB <dania.demidov@yandex.ru>
2020-07-24 01:19:34 +0300

    Some UX changes

--- src/mail/order_mail.py
+++ src/mail/order_mail.py
@@ -7,7 +7,7 @@ from products.gets import formReprData
 
 def sendThanksForOrderEmail(order_object: Order):
     orderRepresentation = formReprData(json.loads(order_object.json), telegramMarkdown = False)
-    sendMail(f"{order_object.name}, спасибо за заказ!", f"Номер заказа: №{order_object.id}\n\nВы заказали:\n{orderRepresentation}", order_object.email)
+    sendMail(f"{order_object.name}, спасибо за заказ!", f"Номер заказа: №{order_object.id}\n\nВы заказали:\n{orderRepresentation}\n\nДля получения заказа в магазине назовите консультанту его номер.", order_object.email)
 
 def sendCancelledOrderEmail(order_object: Order, oid):
     sendMail(f"Отмена заказа №{oid}", f"{order_object.name}, Ваш заказ отменён. Если это сделали не Вы, значит, его отменила администрация.\nЕсли у Вас есть вопросы, позвоните по тел. 44-51-62.", order_object.email)
